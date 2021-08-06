286136986d3b6b9e40b10213b7abee9fa215b2b3
Tamer <tamr.i.b99@gmail.com>
2021-08-06 16:29:27 +0300

    add todo list

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -5,6 +5,7 @@ import f from '../particles.json';
 import OnlineShopping from '../../Images/online-store-compressor.png';
 import ItalianPizza from '../../Images/italian-pizza-compressor.png';
 import Furniture from '../../Images/furniture-compressor.png';
+import TodoList from '../../Images/todo-list.png';
 
 
 
@@ -18,7 +19,7 @@ function Portfolio() {
                 <div className="box">
                   <div className="row">
 
-                      <div className="col-sm-6 col-lg-4">
+                      <div className="col-sm-6 col-lg-4 col-xl-3">
                           <div className="sec animate__animated animate__jackInTheBox">
                                <img src={OnlineShopping} alt="online-shopping" />
                                <h4>Online Shopping</h4>
@@ -26,7 +27,7 @@ function Portfolio() {
                           </div>
                       </div>
 
-                      <div className="col-sm-6 col-lg-4">
+                      <div className="col-sm-6 col-lg-4 col-xl-3">
                           <div className="sec animate__animated animate__jackInTheBox">
                           <img src={ItalianPizza} alt="italian-pizza" />
                                <h4>Italian Pizza</h4>
@@ -34,13 +35,22 @@ function Portfolio() {
                           </div>
                       </div>
 
-                      <div className="col-sm-6 col-lg-4">
+                      <div className="col-sm-6 col-lg-4 col-xl-3">
                           <div className="sec animate__animated animate__jackInTheBox">
                                 <img src={Furniture} alt="furniture" />
                                 <h4>Furniture</h4>
                                 <a href="https://xenodochial-payne-3e7fc1.netlify.app" target="_blank" rel="noopener noreferrer">Check Link!</a>
                           </div>
                       </div>
+
+                      <div className="col-sm-6 col-lg-4 col-xl-3">
+                          <div className="sec animate__animated animate__jackInTheBox">
+                          <img src={TodoList} alt="todo-list" />
+                               <h4>TodoList</h4>
+                               <a href="https://modest-mestorf-f770db.netlify.app/" target="_blank" rel="noopener noreferrer">Check Link!</a>
+                          </div>
+                      </div>
+
                     </div>
                   </div>
            </div>
