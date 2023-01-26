8126723cc7e0ddd58ff30e39a04ad82fb693dca9
Tamer <tamr.i.b99@gmail.com>
2023-01-26 17:32:58 +0200

    some changes in resume page

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -23,15 +23,15 @@ function Resume() {
                             <div className="date">
                                 <i className="fa fa-calendar" aria-hidden="true"></i><span> 01/06/2020 - 31/12/2020 | Training</span>
                             </div>
-                            <p>I trained at a German company (DataRhine) remotely. For six months in a team, where we completed a number of websites, then we got a certificate of experience from this company.</p>
+                            <p>I trained at a German company (DataRhine) remotely. For six months in a team, where we completed a number of websites, then we got a certificate of experience from this company</p>
                           </div>
 
                           <div className="right">
                             <h5>Front-End Developer</h5>
                             <div className="date">
-                                <i className="fa fa-calendar" aria-hidden="true"></i><span> 11/2021 - 11/2022 | Al Istiqlal University</span>
+                                <i className="fa fa-calendar" aria-hidden="true"></i><span> 01/2022 - till today | Al Istiqlal University</span>
                             </div>
-                            <p>One year experience in Front End by working as a Front End Developer at Al Istiqlal University</p>
+                            <p>One year experience in Front End by working as a Front End Developer at Al Istiqlal University in Jericho, Palestine</p>
                           </div>
         
                         </div>
